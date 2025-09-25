fb9364f541a4571ccfe6f0179cbad7fb66a220f4
Islam rabea <rabeaeslam56@gmail.com>
2025-09-25 20:35:59 +0300

    case sensitive in file name subtTotal.js

--- src/js/subTotal.js
+++ src/js/subTotal.js
@@ -2,6 +2,7 @@ function subTotal() {
   let Data = JSON.parse(localStorage.getItem("products")) || [];
   let subTotal = document.querySelector(".subtotal p");
   let sub = 0;
+
   Data.forEach((ele) => {
     let priceTotal = ele.quantity * ele.price;
     sub += priceTotal;
